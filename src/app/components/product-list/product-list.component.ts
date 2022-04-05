8c3b350ce636ccb4e28fa1328cb41aaadbb6ce97
Sophia Gavrila <57912010+sophiagavrila@users.noreply.github.com>
2022-04-05 14:55:26 -0400

    add bootstrap

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,5 +1,5 @@
-import { Product } from './../../common/product';
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,20 +1,21 @@
-import { Product } from './../common/product';
+import { Product } from './../models/product';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map, Observable } from 'rxjs'; // from reactive JS
+import { url } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  private baseUrl = 'http://localhost:8080/api/products';
+  private productUrl = `${url}products`; // == to http://localhost:8080/api/products
 
   constructor(private httpClient: HttpClient) { }
 
   // This method maps the JSON data from Spring Data Rest service to a Product array
   getProductList(): Observable<Product[]> {
-    return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
+    return this.httpClient.get<GetResponse>(this.productUrl).pipe(
       map(response => response._embedded.products)
     );
   }
@@ -22,6 +23,7 @@ export class ProductService {
 
 // This unwraps the JSON from Spring Data REST _embedded entry
 interface GetResponse { // makes use of the _embedded entry that is returned by Spring
+  // inside of the _embedded property are a bunch of objects that match up with the Product model we built in Angular
   _embedded: {
     products: Product[];
   }
